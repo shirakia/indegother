93673ec32d49e905ba020ac7ea16811e4dcf29e1
shirakia <atsuo.shiraki@gmail.com>
2021-06-30 21:21:01 +0900

    Log all DB queries

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
